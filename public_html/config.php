0e4f94ae83f0d1ccc31f8f4b0393f7a75496afae
Darrell Lane <darrell-lane@hotmail.co.uk>
2021-05-30 17:56:21 +0100

    Refactor ( Added missing PHP error mode line )

--- public_html/config.php
+++ public_html/config.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
 
 require(dirname($_SERVER['DOCUMENT_ROOT'], 1) . '/vendor/autoload.php');
 $dotenv = Dotenv\Dotenv::createImmutable(dirname($_SERVER['DOCUMENT_ROOT'], 1));
@@ -11,6 +12,7 @@ $host = $_ENV['HOST'];
 
 try {
     $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
