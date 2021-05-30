ad97c3582d9a46d606ff5c45fc6719bda40fc456
Darrell Lane <darrell-lane@hotmail.co.uk>
2021-05-30 14:49:39 +0100

    Refactor ( Updated the image src to DO cdn )

--- public_html/index.php
+++ public_html/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="./assets/images/google-favicon-512.png" />
+    <link rel="icon" type="image/png" href="https://kreceo.sfo2.digitaloceanspaces.com/Froogle/google-favicon-512.png" />
     <link rel="stylesheet" href="./assets/css/style.css">
     <title>Froogle</title>
 </head>
@@ -13,9 +13,9 @@
     <div class="wrapper indexPage">
         <section class="mainSection">
             <div class="logoContainer">
-                <img src="./assets/images/froogle-logo.png" />
+                <img src="https://kreceo.sfo2.digitaloceanspaces.com/Froogle/froogle-logo.png" />
             </div>
-            <div class="searchContainer">
+            <div class="searchContainer">s
                 <form action="search.php" method="GET">
                     <svg focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
